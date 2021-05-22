13757a02c7d229fd327bea2243e44da8f5a46ec3
Paulo Estraich <69217964+paulocwb@users.noreply.github.com>
2021-05-22 14:06:11 -0300

    updates Fixes #1

--- src/domain/projects/model/project.model.js
+++ src/domain/projects/model/project.model.js
@@ -4,11 +4,25 @@ const tagSchema = mongoose.Schema({
 	name: String,
 });
 
+
+const activities = mongoose.Schema({
+	name: { type: String },
+	url: [{ type: String}],
+	
+})
+
 const projectSchema = mongoose.Schema({
 	_id: { type: String },
 	shortDescription: { type: String },
 	fullDescription: { type: String },
 	links: [{ url: string }],
+	status:{
+		startDate:{ type: Date, default: Date.now},
+		finishDate:{ type: Date},
+		activities:[
+
+		]
+	},
 	university: { type: String, ref: "User" },
 	institute: { type: String, ref: "User", required: true },
 	students: [
