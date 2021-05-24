04894355ec98f1ae85e944aeccfe1aa00b8e4e15
Paulo Estraich <69217964+paulocwb@users.noreply.github.com>
2021-05-23 21:55:37 -0300

    EASDAS

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import { App } from "./app/App";
 import { Database } from "./infra/Database";
 import dbConf from "./config/mongo.config";
 import middleware from "./infra/middleware/global.middleware";
-import * as routes from "./infra/routes/index";
+//import * as routes from "./infra/routes/index";
 
 const db = new Database(dbConf);
 const app = new App();
@@ -13,9 +13,9 @@ const app = new App();
 		Object.values(middleware).forEach(mid =>{
 			app.loadMiddleware(mid);
 		});
-		Object.entries(routes).forEach(route =>{
+		/* Object.entries(routes).forEach(route =>{
 			app.loadRoute(`${process.env.DEFAULT_URL}/${route[0]}`,route[1].default)
-		})
+		}) */
 		
 		app.start(port);
 	});
